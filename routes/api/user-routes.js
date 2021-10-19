34623ec3258225cebea13910d71de88ab3273668
Conley Marie <81729253+conleyannmarie@users.noreply.github.com>
2021-10-19 14:56:34 -0600

    added code snapshot

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -51,28 +51,36 @@ router.post('/', (req, res) => {
       });
   });
 
+  router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+      User.findOne({
+        where: {
+          email: req.body.email
+        }
+      }).then(dbUserData => {
+        if (!dbUserData) {
+          res.status(400).json({ message: 'No user with that email address!' });
+          return;
+        }
+    
+        res.json({ user: dbUserData });
+    
+        // Verify user
+    
+      });  
+    });
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   
-    // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+    // pass in req.body instead to only update what's passed through
     User.update(req.body, {
+      individualHooks: true,
       where: {
         id: req.params.id
       }
     })
-      .then(dbUserData => {
-        if (!dbUserData[0]) {
-          res.status(404).json({ message: 'No user found with this id' });
-          return;
-        }
-        res.json(dbUserData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
 
 // DELETE /api/users/1
 router.delete('/:id', (req, res) => {
