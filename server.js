b4880dcd534bf49e62f7e16410d6096e883ce1f5
Conley Marie <81729253+conleyannmarie@users.noreply.github.com>
2021-10-20 21:54:56 -0600

    added code snapshot

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const routes = require('./controllers/');
-const sequelize = require('./config/connection');
 const path = require('path');
-const exphbs = require('express-handlebars');
-//const hbs = exphbs.create({});
-
+const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -21,21 +21,17 @@ const sess = {
 
 app.use(session(sess));
 
-const app = express();
+const hbs = exphbs.create({});
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-const PORT = process.env.PORT || 3001;
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// turn on routes
-app.use(routes);
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// turn on connection to db and server
+app.use(require('./controllers/'));
+
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
